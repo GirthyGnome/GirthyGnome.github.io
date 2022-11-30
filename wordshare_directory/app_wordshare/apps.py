c1aaaeb1185204eea6c760317d15c305721dcc82
GirthyGnome <112988489+GirthyGnome@users.noreply.github.com>
2022-11-29 16:45:39 -0800

    notable content pushed: app.js for the API call, index.html for basic… … testing, django apps created inside wordshare_directory. Django files are encountering an error, potentially due to a name change. Error is 'ImportError: No module named app_wordshare.settings'.

--- wordshare_directory/app_wordshare/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppWordshareConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_wordshare'
--- wordshare_directory/users_wordshare/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsersWordshareConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users_wordshare'
